3eddde4d3b7c6c5e2cafb00335914ac4c4c60bfa
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-05-16 00:29:01 +1200

    init project

---
+++ internal-api/src/test/java/internalapi/InternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package internalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
